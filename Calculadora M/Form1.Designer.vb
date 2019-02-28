a7baea5c824eb0dbda2a3d1173a6c61d0fc29667
AtumaneMomade <betomomade40@gmail.com>
2019-02-28 12:48:56 +0200

    Calculadora
    atumane

---
+++ Calculadora M/Form1.Designer.vb
@@ -0,0 +1,257 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Num1 = New System.Windows.Forms.Button()
+        Me.Num2 = New System.Windows.Forms.Button()
+        Me.Num3 = New System.Windows.Forms.Button()
+        Me.Num4 = New System.Windows.Forms.Button()
+        Me.Num5 = New System.Windows.Forms.Button()
+        Me.Num6 = New System.Windows.Forms.Button()
+        Me.Num7 = New System.Windows.Forms.Button()
+        Me.Num8 = New System.Windows.Forms.Button()
+        Me.Num9 = New System.Windows.Forms.Button()
+        Me.Num0 = New System.Windows.Forms.Button()
+        Me.virgula = New System.Windows.Forms.Button()
+        Me.Resultado = New System.Windows.Forms.Button()
+        Me.Soma = New System.Windows.Forms.Button()
+        Me.btnDivisao = New System.Windows.Forms.Button()
+        Me.Multiplicacao = New System.Windows.Forms.Button()
+        Me.limpar = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.menos = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Num1
+        '
+        Me.Num1.Location = New System.Drawing.Point(12, 296)
+        Me.Num1.Name = "Num1"
+        Me.Num1.Size = New System.Drawing.Size(75, 23)
+        Me.Num1.TabIndex = 0
+        Me.Num1.Text = "1"
+        Me.Num1.UseVisualStyleBackColor = True
+        '
+        'Num2
+        '
+        Me.Num2.Location = New System.Drawing.Point(93, 296)
+        Me.Num2.Name = "Num2"
+        Me.Num2.Size = New System.Drawing.Size(75, 23)
+        Me.Num2.TabIndex = 1
+        Me.Num2.Text = "2"
+        Me.Num2.UseVisualStyleBackColor = True
+        '
+        'Num3
+        '
+        Me.Num3.Location = New System.Drawing.Point(174, 296)
+        Me.Num3.Name = "Num3"
+        Me.Num3.Size = New System.Drawing.Size(75, 23)
+        Me.Num3.TabIndex = 2
+        Me.Num3.Text = "3"
+        Me.Num3.UseVisualStyleBackColor = True
+        '
+        'Num4
+        '
+        Me.Num4.Location = New System.Drawing.Point(12, 256)
+        Me.Num4.Name = "Num4"
+        Me.Num4.Size = New System.Drawing.Size(75, 23)
+        Me.Num4.TabIndex = 3
+        Me.Num4.Text = "4"
+        Me.Num4.UseVisualStyleBackColor = True
+        '
+        'Num5
+        '
+        Me.Num5.Location = New System.Drawing.Point(93, 256)
+        Me.Num5.Name = "Num5"
+        Me.Num5.Size = New System.Drawing.Size(75, 23)
+        Me.Num5.TabIndex = 4
+        Me.Num5.Text = "5"
+        Me.Num5.UseVisualStyleBackColor = True
+        '
+        'Num6
+        '
+        Me.Num6.Location = New System.Drawing.Point(174, 256)
+        Me.Num6.Name = "Num6"
+        Me.Num6.Size = New System.Drawing.Size(75, 23)
+        Me.Num6.TabIndex = 5
+        Me.Num6.Text = "6"
+        Me.Num6.UseVisualStyleBackColor = True
+        '
+        'Num7
+        '
+        Me.Num7.Location = New System.Drawing.Point(12, 217)
+        Me.Num7.Name = "Num7"
+        Me.Num7.Size = New System.Drawing.Size(75, 23)
+        Me.Num7.TabIndex = 6
+        Me.Num7.Text = "7"
+        Me.Num7.UseVisualStyleBackColor = True
+        '
+        'Num8
+        '
+        Me.Num8.Location = New System.Drawing.Point(93, 217)
+        Me.Num8.Name = "Num8"
+        Me.Num8.Size = New System.Drawing.Size(75, 23)
+        Me.Num8.TabIndex = 7
+        Me.Num8.Text = "8"
+        Me.Num8.UseVisualStyleBackColor = True
+        '
+        'Num9
+        '
+        Me.Num9.Location = New System.Drawing.Point(174, 217)
+        Me.Num9.Name = "Num9"
+        Me.Num9.Size = New System.Drawing.Size(75, 23)
+        Me.Num9.TabIndex = 8
+        Me.Num9.Text = "9"
+        Me.Num9.UseVisualStyleBackColor = True
+        '
+        'Num0
+        '
+        Me.Num0.Location = New System.Drawing.Point(12, 334)
+        Me.Num0.Name = "Num0"
+        Me.Num0.Size = New System.Drawing.Size(75, 23)
+        Me.Num0.TabIndex = 9
+        Me.Num0.Text = "0"
+        Me.Num0.UseVisualStyleBackColor = True
+        '
+        'virgula
+        '
+        Me.virgula.Location = New System.Drawing.Point(93, 334)
+        Me.virgula.Name = "virgula"
+        Me.virgula.Size = New System.Drawing.Size(75, 23)
+        Me.virgula.TabIndex = 10
+        Me.virgula.Text = "."
+        Me.virgula.UseVisualStyleBackColor = True
+        '
+        'Resultado
+        '
+        Me.Resultado.Location = New System.Drawing.Point(174, 334)
+        Me.Resultado.Name = "Resultado"
+        Me.Resultado.Size = New System.Drawing.Size(75, 23)
+        Me.Resultado.TabIndex = 11
+        Me.Resultado.Text = "="
+        Me.Resultado.UseVisualStyleBackColor = True
+        '
+        'Soma
+        '
+        Me.Soma.Location = New System.Drawing.Point(268, 217)
+        Me.Soma.Name = "Soma"
+        Me.Soma.Size = New System.Drawing.Size(75, 23)
+        Me.Soma.TabIndex = 12
+        Me.Soma.Text = "+"
+        Me.Soma.UseVisualStyleBackColor = True
+        '
+        'btnDivisao
+        '
+        Me.btnDivisao.Location = New System.Drawing.Point(268, 256)
+        Me.btnDivisao.Name = "btnDivisao"
+        Me.btnDivisao.Size = New System.Drawing.Size(75, 23)
+        Me.btnDivisao.TabIndex = 13
+        Me.btnDivisao.Text = "/"
+        Me.btnDivisao.UseVisualStyleBackColor = True
+        '
+        'Multiplicacao
+        '
+        Me.Multiplicacao.Location = New System.Drawing.Point(268, 334)
+        Me.Multiplicacao.Name = "Multiplicacao"
+        Me.Multiplicacao.Size = New System.Drawing.Size(75, 23)
+        Me.Multiplicacao.TabIndex = 15
+        Me.Multiplicacao.Text = "*"
+        Me.Multiplicacao.UseVisualStyleBackColor = True
+        '
+        'limpar
+        '
+        Me.limpar.Location = New System.Drawing.Point(268, 179)
+        Me.limpar.Name = "limpar"
+        Me.limpar.Size = New System.Drawing.Size(75, 23)
+        Me.limpar.TabIndex = 16
+        Me.limpar.Text = "C"
+        Me.limpar.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(12, 118)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(331, 38)
+        Me.TextBox1.TabIndex = 17
+        Me.TextBox1.Text = "0.00"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'menos
+        '
+        Me.menos.Location = New System.Drawing.Point(268, 296)
+        Me.menos.Name = "menos"
+        Me.menos.Size = New System.Drawing.Size(75, 23)
+        Me.menos.TabIndex = 18
+        Me.menos.Text = "-"
+        Me.menos.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(371, 373)
+        Me.Controls.Add(Me.menos)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.limpar)
+        Me.Controls.Add(Me.Multiplicacao)
+        Me.Controls.Add(Me.btnDivisao)
+        Me.Controls.Add(Me.Soma)
+        Me.Controls.Add(Me.Resultado)
+        Me.Controls.Add(Me.virgula)
+        Me.Controls.Add(Me.Num0)
+        Me.Controls.Add(Me.Num9)
+        Me.Controls.Add(Me.Num8)
+        Me.Controls.Add(Me.Num7)
+        Me.Controls.Add(Me.Num6)
+        Me.Controls.Add(Me.Num5)
+        Me.Controls.Add(Me.Num4)
+        Me.Controls.Add(Me.Num3)
+        Me.Controls.Add(Me.Num2)
+        Me.Controls.Add(Me.Num1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Num1 As Button
+    Friend WithEvents Num2 As Button
+    Friend WithEvents Num3 As Button
+    Friend WithEvents Num4 As Button
+    Friend WithEvents Num5 As Button
+    Friend WithEvents Num6 As Button
+    Friend WithEvents Num7 As Button
+    Friend WithEvents Num8 As Button
+    Friend WithEvents Num9 As Button
+    Friend WithEvents Num0 As Button
+    Friend WithEvents virgula As Button
+    Friend WithEvents Resultado As Button
+    Friend WithEvents Soma As Button
+    Friend WithEvents btnDivisao As Button
+    Friend WithEvents Multiplicacao As Button
+    Friend WithEvents limpar As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents menos As Button
+End Class
---
+++ Calculadora M/Form1.vb
@@ -0,0 +1,234 @@
+﻿Public Class Form1
+
+    Private dblResultado As Double = 0
+    Private dblNumero As Double = 0
+    Private strOperador As String = String.Empty
+    Private blnLimpa As Boolean = True
+    Private blnDecimal As Boolean = False
+    Private strNumero As String = "1"
+
+    Private blnOperador As Boolean = False
+
+    Private Sub btnDivisao_Click(sender As Object, e As EventArgs) Handles btnDivisao.Click
+
+        If strNumero = "1" And blnOperador = True Then
+            dblResultado = Convert.ToDouble(TextBox1.Text)
+        ElseIf strNumero = "2" And blnOperador = True Then
+            dblResultado = Convert.ToDouble(TextBox1.Text)
+            dblResultado = dblResultado / dblNumero
+            TextBox1.Text = dblNumero
+            dblNumero = 0
+        End If
+        strOperador = "/"
+        blnLimpa = True
+        blnDecimal = False
+        blnOperador = False
+    End Sub
+
+    Private Sub limpar_Click(sender As Object, e As EventArgs) Handles limpar.Click
+        dblNumero = 0
+        dblResultado = 0
+        strOperador = String.Empty
+        TextBox1.Text = "0"
+        blnLimpa = True
+    End Sub
+
+    Private Sub Multiplicacao_Click(sender As Object, e As EventArgs) Handles Multiplicacao.Click
+        If strNumero = "1" And blnOperador = True Then
+            dblResultado = Convert.ToDouble(TextBox1.Text)
+
+
+        ElseIf strNumero = "2" And blnOperador = True Then
+            dblResultado = Convert.ToDouble(TextBox1.Text)
+            dblResultado = dblResultado * dblNumero
+            TextBox1.Text = dblNumero
+            dblNumero = 0
+        End If
+        strOperador = "*"
+        blnLimpa = True
+        blnDecimal = False
+        blnOperador = False
+    End Sub
+
+    Private Sub Num1_Click(sender As Object, e As EventArgs) Handles Num1.Click
+        If blnLimpa = True Then
+            TextBox1.Text = "1"
+            blnLimpa = False
+
+
+        Else
+            TextBox1.Text = TextBox1.Text & "1"
+        End If
+        blnOperador = True
+    End Sub
+
+    Private Sub Num0_Click(sender As Object, e As EventArgs) Handles Num0.Click
+        If blnLimpa = True Then
+            TextBox1.Text = "0"
+            blnLimpa = False
+
+
+
+        Else
+            TextBox1.Text = TextBox1.Text & "0"
+        End If
+        blnOperador = True
+    End Sub
+
+    Private Sub Num2_Click(sender As Object, e As EventArgs) Handles Num2.Click
+        If blnLimpa = True Then
+            TextBox1.Text = "2"
+            blnLimpa = False
+
+
+        Else
+            TextBox1.Text = TextBox1.Text & "2"
+        End If
+        blnOperador = True
+
+    End Sub
+
+    Private Sub Num3_Click(sender As Object, e As EventArgs) Handles Num3.Click
+        If blnLimpa = True Then
+            TextBox1.Text = "3"
+            blnLimpa = False
+
+
+
+        Else
+            TextBox1.Text = TextBox1.Text & "3"
+        End If
+        blnOperador = True
+    End Sub
+
+    Private Sub Num4_Click(sender As Object, e As EventArgs) Handles Num4.Click
+        If blnLimpa = True Then
+            TextBox1.Text = "4"
+            blnLimpa = False
+
+
+
+        Else
+            TextBox1.Text = TextBox1.Text & "4"
+        End If
+        blnOperador = True
+    End Sub
+
+    Private Sub Num5_Click(sender As Object, e As EventArgs) Handles Num5.Click
+        If blnLimpa = True Then
+            TextBox1.Text = "5"
+            blnLimpa = False
+
+
+        Else
+            TextBox1.Text = TextBox1.Text & "5"
+        End If
+        blnOperador = True
+    End Sub
+
+    Private Sub Num6_Click(sender As Object, e As EventArgs) Handles Num6.Click
+        If blnLimpa = True Then
+            TextBox1.Text = "6"
+            blnLimpa = False
+
+
+        Else
+            TextBox1.Text = TextBox1.Text & "6"
+        End If
+        blnOperador = True
+    End Sub
+
+    Private Sub Num7_Click(sender As Object, e As EventArgs) Handles Num7.Click
+        If blnLimpa = True Then
+            TextBox1.Text = "7"
+            blnLimpa = False
+
+
+        Else
+            TextBox1.Text = TextBox1.Text & "7"
+        End If
+        blnOperador = True
+    End Sub
+
+    Private Sub Num8_Click(sender As Object, e As EventArgs) Handles Num8.Click
+        If blnLimpa = True Then
+            TextBox1.Text = "8"
+            blnLimpa = False
+
+
+        Else
+            TextBox1.Text = TextBox1.Text & "8"
+        End If
+        blnOperador = True
+    End Sub
+
+    Private Sub Num9_Click(sender As Object, e As EventArgs) Handles Num9.Click
+        If blnLimpa = True Then
+            TextBox1.Text = "9"
+            blnLimpa = False
+
+
+        Else
+            TextBox1.Text = TextBox1.Text & "9"
+        End If
+        blnOperador = True
+    End Sub
+
+    Private Sub Soma_Click(sender As Object, e As EventArgs) Handles Soma.Click
+        If strNumero = "1" And blnOperador = True Then
+            dblResultado = Convert.ToDouble(TextBox1.Text)
+
+        ElseIf strNumero = "2" And blnOperador = True Then
+            dblResultado = Convert.ToDouble(TextBox1.Text)
+            dblResultado = dblResultado + dblNumero
+            TextBox1.Text = dblNumero
+            dblNumero = 0
+        End If
+        strOperador = "+"
+        blnLimpa = True
+        blnDecimal = False
+        blnOperador = False
+    End Sub
+
+    Private Sub menos_Click(sender As Object, e As EventArgs) Handles menos.Click
+
+
+        If strNumero = "1" And blnOperador = True Then
+
+
+            dblResultado = Convert.ToDouble(TextBox1.Text)
+
+        ElseIf strNumero = "2" And blnOperador = True Then
+            dblResultado = Convert.ToDouble(TextBox1.Text)
+            dblResultado = dblResultado - dblNumero
+            TextBox1.Text = dblNumero
+            dblNumero = 0
+        End If
+        strOperador = "-"
+        blnLimpa = True
+        blnDecimal = False
+        blnOperador = False
+    End Sub
+
+    Private Sub virgula_Click(sender As Object, e As EventArgs) Handles virgula.Click
+        If blnDecimal = False Then
+            TextBox1.Text = TextBox1.Text & "."
+            blnDecimal = True
+        End If
+    End Sub
+
+    Private Sub Resultado_Click(sender As Object, e As EventArgs) Handles Resultado.Click
+        If strOperador = "+" And dblNumero = 0 Then
+            dblResultado = dblResultado + Convert.ToDouble(TextBox1.Text)
+        ElseIf strOperador = "-" And dblNumero = 0 Then
+            dblResultado = dblResultado - Convert.ToDouble(TextBox1.Text)
+        ElseIf strOperador = "*" And dblNumero = 0 Then
+            dblResultado = dblResultado * Convert.ToDouble(TextBox1.Text)
+        ElseIf strOperador = "/" And dblNumero = 0 Then
+            dblResultado = dblResultado / Convert.ToDouble(TextBox1.Text)
+        End If
+        TextBox1.Text = dblResultado
+        blnLimpa = True
+        blnOperador = False
+    End Sub
+End Class
